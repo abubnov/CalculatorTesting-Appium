7e0f777ebfb6fd9b68add9673be34a646303339d
Andrey Bubnov <Andrey.Bubnov@dataart.com>
2021-02-07 22:33:04 +0100

    Updated test for Calculator.

--- src/test/java/CalculatorSmokeTests.java
+++ src/test/java/CalculatorSmokeTests.java
@@ -42,28 +42,41 @@ public class CalculatorSmokeTests {
     }
 
 
+    //Sum testing 7+3=10
     @Test
-    public void testCalculator() throws Exception {
-        Thread.sleep(5000);
+    public void sumTest() throws Exception {
+        enterNumber("7");
+        // addition +
+        addition();
+        enterNumber("3");
+        //equals =
+        equals();
+        String resultA = result();
+        assertResult(resultA, "10");
+        assert resultA.equals("10") : "Actual value is : " + resultA + " did not match with expected value: 10";
 
-        WebElement seven = driver.findElementById("com.android.calculator2:id/digit_7");
-        seven.click();
-        WebElement plus = driver.findElementById("com.android.calculator2:id/op_add");
-        plus.click();
-        WebElement three = driver.findElementById("com.android.calculator2:id/digit_3");
-        three.click();
-        WebElement equalTo = driver.findElementById("com.android.calculator2:id/eq");
-        equalTo.click();
 
-        WebElement formulaElement = driver.findElementById("com.android.calculator2:id/formula");
+    }
 
-        Thread.sleep(2000);
+    //Multiplication testing 12345679*9=111111111
+    @Test
+    public void multipleTest() throws Exception {
+        enterNumber("1");
+        enterNumber("2");
+        enterNumber("3");
+        enterNumber("4");
+        enterNumber("5");
+        enterNumber("6");
+        enterNumber("7");
+        enterNumber("9");
+        multiply();
+        enterNumber("9");
+        equals();
 
-        WebElement results = driver.findElementById("com.android.calculator2:id/result");
+        String resultB = result();
+        assertResult(resultB, "111,111,111");
 
-        assertResult(results, "10");
-
-        assert results.getText().equals("10") : "Actual value is : " + results.getText() + " did not match with expected value: 10";
+        assert resultB.equals("111,111,111") : "Actual value is : " + resultB + " did not match with expected value: 10";
 
     }
 
@@ -72,16 +85,77 @@ public class CalculatorSmokeTests {
         driver.quit();
     }
 
-    public void assertResult(WebElement resultElement, String resultValue) {
+    public void assertResult(String actualResult, String expectedResulte) {
 
-        if (resultElement.getText().equals(resultValue)) {
+        if (actualResult.equals(expectedResulte)) {
             System.out.println("Test Passed...");
-            System.out.println("AR is " + resultElement.getText());
-            System.out.println("ER is " + resultValue);
+            System.out.println("AR is " + actualResult);
+            System.out.println("ER is " + expectedResulte);
         } else {
             System.out.println("Test Failed...");
-            System.out.println("AR is " + resultElement.getText());
-            System.out.println("ER is " + resultValue);
+            System.out.println("AR is " + actualResult);
+            System.out.println("ER is " + expectedResulte);
         }
     }
+
+    public void enterNumber(String value) {
+        WebElement numberElement = driver.findElementById("com.android.calculator2:id/digit_" + value);
+        numberElement.click();
+    }
+
+    //Click on Sum element +
+    public void addition() {
+        WebElement plus = driver.findElementById("com.android.calculator2:id/op_add");
+        plus.click();
+    }
+
+    //Click on Minus element -
+    public void minus() {
+        WebElement plus = driver.findElementById("com.android.calculator2:id/op_sub");
+        plus.click();
+    }
+
+    //Click on Multiply element *
+    public void multiply() {
+        WebElement plus = driver.findElementById("com.android.calculator2:id/op_mul");
+        plus.click();
+    }
+
+    //Click on Divide element /
+    public void divide() {
+        WebElement plus = driver.findElementById("com.android.calculator2:id/op_div");
+        plus.click();
+    }
+
+    //Click on DEL element *
+    public void del() {
+        WebElement plus = driver.findElementById("com.android.calculator2:id/del");
+        plus.click();
+    }
+
+    //Click on Dec Point element .
+    public void decPoint() {
+        WebElement plus = driver.findElementById("com.android.calculator2:id/dec_point");
+        plus.click();
+    }
+
+    //Click on Equal element =
+    public void equals() {
+        WebElement equalTo = driver.findElementById("com.android.calculator2:id/eq");
+        equalTo.click();
+    }
+
+    public String result() {
+        WebElement results = driver.findElementById("com.android.calculator2:id/result");
+        String resultText = results.getText();
+        return resultText;
+    }
+
+    public String formula() {
+        WebElement formulaElement = driver.findElementById("com.android.calculator2:id/formula");
+        String formulaText = formulaElement.getText();
+        return formulaText;
+    }
+
+
 }
